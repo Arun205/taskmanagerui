2c39f5144b184bae70b5670daddf6e305ceeee6d
Kulanthaivel <539617@cognizant.com>
2019-03-27 14:14:30 +0530

    view task updated

--- src/app/viewtask/viewtask.component.ts
+++ src/app/viewtask/viewtask.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewtaskComponent implements OnInit {
 
+  public tasks =  [
+        {'task': 'Task 1', 'parentTask': 'Parent Task 1', 'start': '2018/01/01', 'end':'2018/01/01', 'priority':10},
+        {'task': 'Task 2', 'parentTask': 'Parent Task 2', 'start': '2018/01/01', 'end':'2018/01/01', 'priority':10},
+        {'task': 'Task 3', 'parentTask': 'Parent Task 3', 'start': '2018/01/01', 'end':'2018/01/01', 'priority':10}
+    ]
+
   constructor() { }
 
   ngOnInit() {
